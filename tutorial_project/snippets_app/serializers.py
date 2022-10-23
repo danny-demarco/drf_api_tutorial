e175f63096962cc3008c37bc83d67a1896be4d78
danny-demarco <metamorphosism@protonmail.com>
2022-10-23 20:14:01 +0100

    serializer class  and ModelSerializer class defined

--- tutorial_project/snippets_app/serializers.py
+++ tutorial_project/snippets_app/serializers.py
@@ -11,34 +11,43 @@ from snippets_app.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
     This is particularly useful for controlling how the browsable API should be displayed.
 """
 
-class SnippetSerializer(serializers.Serializer):
-    """
-        Defines the fields to be serialized and/or deserialized
-    """
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False)
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+# class SnippetSerializer(serializers.Serializer):
+#     """
+#         Defines the fields to be serialized and/or deserialized
+#     """
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#     code = serializers.CharField(style={'base_template': 'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
 
 
-    def create(self, validated_data):
-        """
-            Create and return a new `Snippet` instance, given the validated data
-        """
+#     def create(self, validated_data):
+#         """
+#             Create and return a new `Snippet` instance, given the validated data
+#         """
 
-        return Snippet.objects.create(**validated_data)
+#         return Snippet.objects.create(**validated_data)
 
 
-    def update(self, instance, validated_data):
-        """
-            Update and return existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         """
+#             Update and return existing `Snippet` instance, given the validated data.
+#         """
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.linenos = validated_data.get('linenos', instance.linenos)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.style = validated_data.get('style', instance.style)
+#         instance.save()
+#         return instance
+
+
+# Class Based Serializer saves rewriting out the model data again
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        
